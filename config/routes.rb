ff60ac6d5c9ee01f73250a9a511b44425bdd47d7
ben20262 <ben20262@aim.com>
2020-10-18 22:17:09 -0400

    removed excess resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :create, :destroy]
-
-  resources :sellers, only: [:index, :show, :create, :destory]
-
-  resources :users, only: [:index, :create]
 
   get '/auth/facebook/callback' => 'sessions#face_create'
-  get '/settings', to: 'users#settings', as: 'settings'
-  get '/users/:id/posts', to: 'users#user_posts', as: 'user_posts'
+  get '/sellers' => 'sellers#index'
 
-  root to: 'users#index'
+  root to: 'posts#index'
 
 end
