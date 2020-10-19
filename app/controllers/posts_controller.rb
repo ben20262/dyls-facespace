dffe1cee6a1f746cedc4f39f4d4005447c9fdbed
ben20262 <ben20262@aim.com>
2020-10-18 22:15:53 -0400

    simplified paths

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,13 @@
 class PostsController < ApplicationController
 
     def index
-        @posts = Post.all
-    end
-
-    def show
-        @post = Post.find_by(id: params[:id])
+        post = Post.scrape
+        
+        render json: post
     end
 
     def create
-        @post = Post.create(title: params[:title], content: params[:content], link: params[:link], price: params[:price], seller_id: params[:seller_id])
+        post = Post.create(title: params[:title], content: params[:content], link: params[:link], price: params[:price], seller_id: params[:seller_id])
     end
 
     def destroy
