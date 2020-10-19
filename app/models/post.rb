8859d94b0e315ee00f8dadfbe97abe20568049a0
ben20262 <ben20262@aim.com>
2020-10-18 22:16:46 -0400

    added scraper

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
   belongs_to :seller
   has_many :user_posts
   has_many :users, through: :user_posts
+
+  def self.scrape
+
+    cards = Nokogiri::HTML(open('https://www.facebook.com/marketplace/114524681898218/cars'))
+    binding.pry
+    cards
+  end
 end
